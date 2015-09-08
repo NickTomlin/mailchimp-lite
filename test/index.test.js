ab959e9d99821c72ccd9541708953ba3211fc440
Nick Tomlin <nick.tomlin@gmail.com>
2015-09-08 17:59:22 -0500

    chore: Add tests for v3 api

--- test/index.test.js
+++ test/index.test.js
@@ -2,14 +2,63 @@ import MailChimp from '../src/index';
 import nock from 'nock';
 
 describe('MailChimp Lite', function () {
-  it('requires a mailchimp api key');
-  it('requires a mailchimp data center');
+  let mailchimp;
 
-  describe('v3 methods', function () {
-
+  beforeEach(function () {
+    mailchimp = new MailChimp({
+      key: 'fakeKey',
+      datacenter: 'us11'
+    });
   });
 
-  describe('v2 batch methods', function () {
+  it('requires a mailchimp api key', () => {
+    expect(() => {
+      let newChimp = new MailChimp({
+        dataCenter: 'us11'
+      })
+    }).to.throw(/Mailchimp API Key Required/);
+  });
 
+  it('requires a mailchimp data center', () => {
+    expect(() => {
+      let newChimp = new MailChimp({
+        key: 'Foo'
+      })
+    }).to.throw(/Mailchimp DataCenter Required/);
+  });
+
+  describe('v3 api', () => {
+    let api;
+    const V3_METHODS = ['get', 'put', 'post', 'patch', 'delete'];
+
+    beforeEach(() => {
+      api = nock('https://us11.api.mailchimp.com/3.0');
+    });
+
+    it('sends basic auth headers', () => {
+      let expectation = api
+        .get('/lists/my-list')
+        .basicAuth({user: 'apikey', pass: 'fakeKey'})
+        .reply(200);
+
+        mailchimp.get('/lists/my-list');
+
+        expectation.done();
+    });
+
+    it('rejects for non-successful status codes');
+    it('rejects for connection errors');
+
+    V3_METHODS.forEach(function (method) {
+      it(`Supports ${method.toUpperCase()}`, () => {
+        let expectation = api[method]('/lists/my-list')
+        .basicAuth({user: 'apikey', pass: 'fakeKey'})
+        .reply(200);
+
+        mailchimp[method]('/lists/my-list');
+
+        expectation.done();
+      });
+    });
   });
 });
